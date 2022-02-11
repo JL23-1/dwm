914fcd4d337e9dc85d6ce2164155401f2f8a60c9
JL23 <71352937+JL23-1@users.noreply.github.com>
2022-02-10 22:47:32 -0500

    changed some colours

--- config.h
+++ config.h
@@ -6,11 +6,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "fontawesome:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#72296c";
+static const char col_gray1[]       = "#0a0b07";
+static const char col_gray2[]       = "#12140e";
+static const char col_gray3[]       = "#1b1e16";
+static const char col_gray4[]       = "#c8d7af";
+static const char col_cyan[]        = "#479681";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
